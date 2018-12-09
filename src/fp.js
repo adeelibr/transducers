c97ba7e8004d3b639a6c9a48201886cae7930922
adeelibr <adeelimranr@gmail.com>
2018-12-10 00:13:35 +0500

    broke fp.js into more

--- src/fp.js
+++ src/fp.js
@@ -1,6 +1,15 @@
 // Utils
 const apply = (x, f) => f(x);
 
-export const compose = (...funcs) => x => funcs.reduceRight(apply, x);
+/**
+ * @param (...funcs) [Array] of functions passed
+ * @param x [Current value] in the array
+ */
+export const compose = (...funcs) => x => {
+  // return funcs.reduceRight(apply, x);
+  return funcs.reduceRight((accumulator, currentFunction) => {
+    return currentFunction(accumulator);
+  }, x);
+};
 
 export const concat = (xs, val) => xs.concat(val);
